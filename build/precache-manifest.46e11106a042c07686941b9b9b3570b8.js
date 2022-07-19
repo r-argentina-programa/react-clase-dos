e478335d6702605a4b4a2bba419fba3245408d85
redotomi <tomy.redondo001@gmail.com>
2022-07-19 17:17:59 -0300

    Crea archivos de build

---
+++ build/precache-manifest.46e11106a042c07686941b9b9b3570b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9439853cd65ab8e70600639d0f47b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6301e6300ef31a5a5ea0",
+    "url": "/static/css/main.930125e9.chunk.css"
+  },
+  {
+    "revision": "b29a3c8b5efa02b0f6b8",
+    "url": "/static/js/2.f95b8323.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.f95b8323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6301e6300ef31a5a5ea0",
+    "url": "/static/js/main.2bc188d3.chunk.js"
+  },
+  {
+    "revision": "5587c03d9b4de7b870e8",
+    "url": "/static/js/runtime-main.a420a0fc.js"
+  }
+]);
